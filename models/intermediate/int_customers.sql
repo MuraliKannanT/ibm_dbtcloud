be7aa81ee0c374fffc89a3a9208f3eca066a5217
Murali Kannan <muralikannan25@gmail.com>
2025-11-25 17:05:14 +0000

    Commit for Job-01

--- models/intermediate/int_customers.sql
+++ models/intermediate/int_customers.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='table')}}
 with cust as 
 (select * from {{ ref('stg_customers')}} ),
 nation as 
@@ -5,6 +6,9 @@ nation as
 region as
 (select * from {{ref('stg_regions')}})
 
-select * from cust 
+select cust.* exclude (comment) rename name as cust_name, 
+nation.name as nation_name,
+region.region_id, region.name as region_name 
+from cust 
 join nation on cust.nation_id = nation.nation_id
 join region on nation.region_id = region.region_id
